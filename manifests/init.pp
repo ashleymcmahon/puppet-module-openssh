1a305869e24e4b10ceae6db3b29c28bb32bf2813
ashleymcmahon <amcmahon@amadeus.com>
2015-06-24 14:53:55 +1000

    still trying

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,21 +17,4 @@ case $operatingsystem {
   package { ['openssh']:
     ensure  => 'installed'
   }
-
-  service { ['sshd']:
-    ensure  => 'running',
-    enable  => true,
-    require => [Package['openssh']]
-  }
-	
-  file { '/etc/ssh/sshd_config':
-    ensure => 'present',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0600',
-    source => "puppet:///modules/${module_name}/sshd_config",
-    notify => Service['sshd']
-  }
-
 }
-
