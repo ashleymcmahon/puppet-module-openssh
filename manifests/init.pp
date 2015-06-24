f41f91dcaa0c799da6f1bdca2c03440646fafc46
Ashley McMahon <amcmahon@amadeus.com>
2015-06-24 14:31:54 +1000

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+# == Class: openssh
+#
+# This class relates to openssh setup/configuration
+#
+# === Parameters
+#
+# No parameters needed
+#
+# === Variables
+#
+# No variables needed
+#
+# === Authors
+#
+# Ashley McMahon  <amcmahon@amadeus.com>
+#
+class ssh {
+
+  package { ['openssh']:
+    ensure  => 'installed',
+    provider => 'zypper']
+  }
+
+  service { ['sshd']:
+    ensure  => 'running',
+    enable  => true,
+    require => [Package['openssh']]
+  }
+	
+  file { '/etc/ssh/sshd_config':
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0600',
+    source => "puppet:///modules/${module_name}/sshd_config",
+    notify => Service['sshd']
+  }
+
+}
