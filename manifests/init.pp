7e9a129e8ef6f1b82aa03b9def17a6ba515ecbe5
ashleymcmahon <amcmahon@amadeus.com>
2015-06-24 14:49:27 +1000

    try again

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,18 @@
-# == Class: openssh
+# == Class: ssh
 #
 # This class relates to openssh setup/configuration
-#
 # === Parameters
-#
 # No parameters needed
-#
 # === Variables
-#
 # No variables needed
-#
 # === Authors
-#
 # Ashley McMahon  <amcmahon@amadeus.com>
-#
+
 class ssh {
+
+case $operatingsystem {
+  suse: {Package{ provider => zypper }}
+}
 
   package { ['openssh']:
     ensure  => 'installed'
