d7ed6ef037ce1d0e743603b503e05edd696d2b8b
ashleymcmahon <amcmahon@amadeus.com>
2015-06-25 13:09:28 +1000

    changed class name

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,13 +8,26 @@
 # === Authors
 # Ashley McMahon  <amcmahon@amadeus.com>
 
-class ssh {
+class openssh {
 
-case $operatingsystem {
-  suse: {Package{ provider => zypper }}
-}
+    package { ['openssh']:
+      ensure  => 'installed',
+      require => Yumrepo['epel']
+    }
 
-  package { ['openssh']:
-    ensure  => 'installed'
+    service { ['sshd']:
+      ensure  => 'running',
+      enable  => true,
+      require => [Package['openssh']]
+    }
+	
+    file { '/etc/ssh/sshd_config':
+      ensure => 'present',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0600',
+      source => "puppet:///modules/${module_name}/sshd_config",
+      notify => Service['sshd']
+    }
+
   }
-}
